630eb0b058261939e74147eac6d995a6e489f174
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-10-03 23:35:34 +0600

    uncomplete

---
+++ Algorithm/predicate_fuction_number _2__and_Binary_search.cpp
@@ -0,0 +1,62 @@
+// predicate2_and_binary_search
+// -----------------------------------------------------
+
+#include<bits/stdc++.h>
+#define ll long long 
+using namespace std;
+ll n,cow;
+vector<ll>a(10000000);
+bool canPlaceCow(ll minimumDistance){////monotonic function return korbe T or F
+    ll lastcowPos = -1;
+    ll cows_count = cow;
+    for(int i=0 ; i<n ; i++){
+        if(a[i]-lastcowPos>=minimumDistance ||lastcowPos==-1){
+            cows_count--;
+            lastcowPos=i;
+        }
+        if(cows_count==0){
+            break;
+        }
+    }
+    if(cows_count==0)return true;
+    else return false;
+}
+void I_Am_Here(){
+    cin>>n>>cow;
+    for(int i=0 ; i<n ; i++){
+        cin>>a[i];
+    }
+    sort(a.begin(),a.end());
+    ll high = 1e9,low=0,mid=0;
+    while(high-low>1){
+        mid = (high+low)/2;
+        ///mid jokhon ans hote pare tokhon mid+1/mid-1 kora jabe nah
+        if(canPlaceCow(mid)){
+            low = mid;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    if(canPlaceCow(high)){
+        cout<<high<<endl;
+    }
+    else{
+        cout<<low<<endl;
+    }
+
+}
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        I_Am_Here();
+    }
+}
+// 1
+// 5 3
+// 1
+// 2
+// 8
+// 4
+// 9
