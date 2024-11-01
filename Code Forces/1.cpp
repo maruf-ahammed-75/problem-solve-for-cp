a454db04c409740143e6ce4bc8e429f6f9668fba
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-11-01 23:58:06 +0600

    Test.cpp

--- Code Forces/1.cpp
+++ Code Forces/1.cpp
@@ -1,26 +1,50 @@
+/*************************************************************************************************************************
+**                                            Maruf Ahammed                                            *
+**                                            CSE, University of Asia Pacific                                            *
+**************************************************************************************************************************/
 #include<bits/stdc++.h>
+#define ff first
+#define ss second
 #define ll long long
+//#define pb push_back
+//#define mp make_pair
+#define Y cout << "YES\n"
+#define N cout << "NO\n"
+#define full(x) x.begin(), x.end()
+
 using namespace std;
-int main(){
+
+
+void I_Am_Here(){
     int n;
     cin>>n;
-    int minin=0,mi=10000;
-    int maxin= 0,mx=-1;
-    for(int i=1; i<=n ; i++){
-        int x;
-        cin>>x;
-        if(x>mx){
-            maxin=i;
-            mx=x;
-        }
-        if(x<=mi){
-            minin=i;
-            mi=x;
-        }
-
+    vector<ll>a(n);
+    ll sum=0;
+    for(int i=0 ; i<n ; i++){
+        cin>>a[i];
+        sum+=a[i];
     }
-    int ans = (maxin-1)+(n-minin);
-    if(maxin>minin)ans--;
-    cout<<ans<<endl;
+    sort(full(a));
+    sum/=n;
+    // for(int i=0 ; i<n ; i++){
+    //     cout<<a[i]<<' '
+    //     ;
+    // }
+    ll up = lower_bound(a.begin(),a.end(),(a[n-1]+1)/2) - a.begin();
+    ll low = upper_bound(a.begin(),a.end() , a[(n-1)/2]) - a.begin();
+   
 
+    // cout<<"up = "<<up<<endl;
+    // cout<<"low = "<<low<<endl;
+    cout<<min(low,up)<<endl;
+    // cout<<sum;
+}
+int main(){
+
+    int ts=1;
+    cin >> ts;
+    for(int xyz=1 ; xyz<=ts ; xyz++){
+        I_Am_Here();
+        // cout<<endl;
+    }
 }
--- Code Forces/tempCodeRunnerFile.cpp
+++ Code Forces/tempCodeRunnerFile.cpp
@@ -3,33 +3,46 @@
 **                                            CSE, University of Asia Pacific                                            *
 **************************************************************************************************************************/
 #include<bits/stdc++.h>
-#define ll long long int
+#define ff first
+#define ss second
+#define ll long long
+//#define pb push_back
+//#define mp make_pair
+#define Y cout << "YES\n"
+#define N cout << "NO\n"
+#define full(x) x.begin(), x.end()
+
 using namespace std;
-const ll mod=1e4 + 7;
+
+
 void I_Am_Here(){
-    ll n;
+    int n;
     cin>>n;
-    ll z=0,p=0,ne=0;
-    vector<ll>a(n);
-    for(auto &i:a){
-        cin>>i;
-        if(i==0){
-            z++;
-        }
-        else if(i<0){
-            ne+=i;
-        }
-        else if(i>0){
-            p+=i;
+    int k;
+    cin>>k;
+    if((n+1)/2!=k){
+        cout<<-1<<endl;
+        return;
+    }
+    int x = 1;
+    for(int i=1 ; i<=sqrt(n) ; i++){
+        if(n%i==0){
+            x = i;
         }
     }
-    // cout<<z<<' '<<ne<<' '<<p<<"\n";
-    cout<<z+((p+z)==abs(ne) && p!=0)<<endl;
+    cout<<n/x;
+    cout<<endl;
+    for(int i=1 ; i<=n ; i+=x){
+        cout<<i<<' ';
+    }
+    cout<<endl;
 }
 int main(){
+
     int ts=1;
-    cin>>ts;
+    cin >> ts;
     for(int xyz=1 ; xyz<=ts ; xyz++){
         I_Am_Here();
+        // cout<<endl;
     }
 }
