e09053c472d60365819a1ce911dd70144c41c138
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-11-13 22:22:00 +0600

    priority-queue

---
+++ Algorithm/STL/priority-queue.cpp
@@ -0,0 +1,12 @@
+min heap -> priority queue given minimum value
+
+max heap -> priority queue given maximum value
+
+priority queue is looking for max or min value
+
+
+Max-Heap Priority Queue
+->priority_queue<int> maxHeap;
+
+Min-Heap Priority Queue
+priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
