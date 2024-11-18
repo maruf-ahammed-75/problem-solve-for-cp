20dae04ff21b932c0948b0c9bb8ccf3ec38245f3
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-11-18 22:09:12 +0600

    sub array sum First solution

---
+++ CSES/subarray-sum-1.cpp
@@ -0,0 +1,29 @@
+
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+void I_Am_Here() {
+    ll n,x;
+    cin>>n>>x;
+    vector<int>a(n);
+    for(auto &i:a)cin>>i;
+    map<ll,ll>m;
+    ll sum=0;
+    ll ans=0;
+    m[0]=1;
+    for(int i=0 ; i<n ; i++){
+        sum+=a[i];
+        m[sum]=1;
+        if(m[(sum-x)])ans++;
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+    int ts = 1;
+    // cin >> ts;
+    for (int xyz = 1; xyz <= ts; xyz++) {
+        I_Am_Here();
+    }
+    return 0;
+}
