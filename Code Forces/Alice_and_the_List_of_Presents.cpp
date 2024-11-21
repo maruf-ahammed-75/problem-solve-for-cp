1a4b8feb23fa2bc7d758500f066fcc841af1d4b3
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-11-22 00:43:37 +0600

    With Recursion Code forces Binary Exponentiation

--- Code Forces/Alice_and_the_List_of_Presents.cpp
+++ Code Forces/Alice_and_the_List_of_Presents.cpp
@@ -2,28 +2,28 @@
 #define ll long long
 using namespace std;
 const ll MOD = 1e9+7;
-//calculate a to the power b
-// ll Binary_Exponentiation(ll a, ll b) {
-//     if(b==0) return 1;
-//     if(b%2==1) {
-//         return Binary_Exponentiation(a,b-1)*a%MOD;
-//     } else {
-//         ll b2 = Binary_Exponentiation(a,b/2)%MOD;
-//         return b2*b2%MOD;
-//     }
-// }->this code is fine
-// but recursive is solwer than iterative
-ll Binary_Exponentiation(ll a, ll b) {// so use this in code
-    ll result = 1;
-    while (b > 0) {
-        if (b & 1) { // If b is odd
-            result = (result * a) % MOD; 
-        }
-        a = (a * a) % MOD; // Square a with mod
-        b /= 2; // Divide b by 2
+calculate a to the power b
+ll Binary_Exponentiation(ll a, ll b) {
+    if(b==0) return 1;
+    if(b%2==1) {
+        return Binary_Exponentiation(a,b-1)*a%MOD;
+    } else {
+        ll b2 = Binary_Exponentiation(a,b/2)%MOD;
+        return b2*b2%MOD;
     }
-    return result;
-}
+}//->this code is fine
+// but recursive is solwer than iterative
+// ll Binary_Exponentiation(ll a, ll b) {// so use this in code
+//     ll result = 1;
+//     while (b > 0) {
+//         if (b & 1) { // If b is odd
+//             result = (result * a) % MOD; 
+//         }
+//         a = (a * a) % MOD; // Square a with mod
+//         b /= 2; // Divide b by 2
+//     }
+//     return result;
+// }
 void I_Am_Here() {
     ll box,num;
     cin>>num>>box;
