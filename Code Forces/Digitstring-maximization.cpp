79554ec7784dcaa8f75bb2bbd27eee74cdedfd13
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-12-10 15:13:05 +0600

    I work this problem in last 7h and last night i work at least 3h but i can't solve it yet

--- Code Forces/Digitstring-maximization.cpp
+++ Code Forces/Digitstring-maximization.cpp
@@ -13,14 +13,14 @@ void I_Am_Here() {
     for(int i=0 ; i<s.size() ; i++){
         a[(s[i]-'0')].push_back(i);
     }
-    // for(int i=0 ; i<10 ; i++){
-    //     cout<<i<<" = ";
-    //     for(auto j:a[i]){
-    //         cout<<j<<' ';
-    //     }
-    //     cout<<endl;
-    // }
-    // cout<<endl;
+    for(int i=0 ; i<10 ; i++){
+        cout<<i<<" = ";
+        for(auto j:a[i]){
+            cout<<j<<' ';
+        }
+        cout<<endl;
+    }
+    cout<<endl;
 
     string b="";
     int ind=0;
@@ -32,26 +32,26 @@ void I_Am_Here() {
         bool f=0;
         for(int i=0 ; i<=9 ; i++){
             if(!a[i].empty()){
-                // cout<<i<<" = ";
+                cout<<i<<" = ";
                 int move = a[i][0]-j;
-                // cout<<a[i][0]<<' '<<j<<' '<<move<<endl;
+                cout<<a[i][0]<<' '<<j<<' '<<move<<endl;
                 if(move>=0){
                     m = i-move;
-                    // cout<<"m = "<<m<<endl;
+                    cout<<"m = "<<m<<endl;
                     if(m>0){
                         if(mx<m){
                             f=1;
                             mx = m;
                             ind=i;
                             index=a[i][0];
-                            // cout<<"mx="<<mx<<"  ind="<<ind<<endl;
+                            cout<<"mx="<<mx<<"  ind="<<ind<<endl;
                         }
                     }
                 }
             }
-            // cout<<endl;
+            cout<<endl;
         }
-        // cout<<"max ind = "<<mx<<' '<<ind<<endl;
+        cout<<"max ind = "<<mx<<' '<<ind<<endl;
         
         for(int k=0 ; k<10 ; k++){
             if(!a[k].empty() && f && index>a[k][0]){
@@ -63,24 +63,24 @@ void I_Am_Here() {
             a[ind].erase(a[ind].begin());
         }
         else{
-            for(int i=0 ; i<10 ; i++){
-                if(a[i][0]==j){
-                    a[i].erase(a[i].begin());
-                    b+=(i+'0');
+            for(int x=0 ; x<10 ; x++){
+                if(a[x][0]==j){
+                    a[x].erase(a[x].begin());
+                    b+=(x+'0');
                     break;
                 }
             }
         }
-        // cout<<"b = "<<b<<endl;
-        // for(int k=0 ; k<=9 ; k++){
-        //     if(!a[k].empty()){
-        //         cout<<k<<"= ";
-        //         for(auto l:a[k]){
-        //             cout<<l<<' ';
-        //         }
-        //         cout<<endl;
-        //     }
-        // }
+        cout<<"b = "<<b<<endl;
+        for(int k=0 ; k<=9 ; k++){
+            if(!a[k].empty()){
+                cout<<k<<"= ";
+                for(auto l:a[k]){
+                    cout<<l<<' ';
+                }
+                cout<<endl;
+             }
+        }
     }
     for(int i=0 ; i<10 ; i++){
         for(auto j:a[i]){b+=(i+'0');}
