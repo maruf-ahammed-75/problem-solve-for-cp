3b40fc9cd669e3a7fb2edb5720690cb63d843347
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2024-12-11 22:18:47 +0600

    Using bfs

--- Code Forces/1.cpp
+++ Code Forces/1.cpp
@@ -1,43 +1,83 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define ff first
-#define ss second
-#define full(a) a.begin(),a.end()
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
-void I_Am_Here() {
-    ll n;
-    cin>>n;
-    ll m;
-    cin>>m;
-    vector<int>a(n);
-    for(int i=0 ; i<n ; i++){
-        string s;cin>>s;
-        a[i] = s.size();
+
+typedef long long ll;
+
+const int MAXN = 1e5 + 5;
+vector<int> a[MAXN]; // Adjacency list
+int b[MAXN];         // Cat presence (1 if cat, 0 if not)
+bool vis[MAXN];      // Visited array
+
+ll bfs(ll s, ll m) {
+    queue<pair<ll, ll>> q; // Pair: {current node, consecutive cats count}
+    vis[s] = true;
+    q.push({s, b[s]}); // Start with root node
+    ll ans = 0;
+
+    while (!q.empty()) {
+        auto cur = q.front();
+        q.pop();
+
+        ll node = cur.first;
+        ll cat = cur.second;
+
+        cout<<node<<' '<<cat<<endl;
+
+        if (cat > m){
+            cout<<"conti"<<endl;
+            continue;
+        } // Skip this path if it exceeds the allowed consecutive cats
+
+        bool isLeaf = true;
+
+        for (ll x : a[node]) {
+            if (!vis[x]) {
+                vis[x] = true;
+                isLeaf = false;
+                ll newCat = (b[x] == 1 ? cat + 1 : 0);
+                q.push({x, newCat});
+                cout<<"x = "<<x<<' '<<"cat = "<<newCat<<endl;
+            }
+        }
+        cout<<endl;
+
+        // If it's a leaf and not the root
+        if (isLeaf && node != 1) {
+            ans++;
+        }
+        cout<<ans<<endl;
     }
-    int i=0;
-    ll sum=0,ans=0,count=0;
-    while(sum<=m){
-        sum+=a[i];
-        i++;
-    }
-    if(sum<=m){
-        cout<<i<<endl;
-    }
-    else {
-        cout<<i-1<<endl;
-    }
+
+    return ans;
 }
 
 int main() {
-    // #ifndef ONLINE_JUDGE
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-    // #endif
-    
-    int t=1;
-    cin>>t;
-    while (t--) {
-        I_Am_Here();
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    ll n, m;
+    cin >> n >> m;
+
+    // Read whether vertices have cats
+    for (ll i = 1; i <= n; i++) {
+        cin >> b[i];
     }
+
+    // Read the edges of the tree
+    for (ll i = 0; i < n - 1; i++) {
+        ll x, y;
+        cin >> x >> y;
+        a[x].push_back(y);
+        a[y].push_back(x);
+    }
+
+    // Start BFS from the root (vertex 1)
+    ll result = bfs(1, m);
+
+    cout << result << endl;
     return 0;
 }
---
+++ Code Forces/Kefa-and-Park.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ff first
+#define ss second
+#define full(a) a.begin(),a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+vector<vector<ll>>a(100000+2324);
+vector<bool>vis(a.size(),false);
+vector<int>b(1000000);
+int bfs(ll s,ll m){
+
+    queue<pair<ll,ll>>q;
+    vis[s]=1;
+    q.push({s,b[s]});
+    ll ans=0;
+
+    while(!q.empty()){
+        
+        ll node = q.front().ff;
+        ll cat = q.front().ss;
+        
+        q.pop();
+        // cout<<cat<<' '<<node<<endl;
+        if(cat>m){
+            // cout<<' '<<"continue"<<endl;
+            continue;
+        }
+        bool f=1;
+        
+        for(ll x :a[node]){
+            if(!vis[x]){
+                vis[x] = true;
+                f=0;
+                ll Ncat = (b[x]==1 ? cat+1:0);
+                q.push({x,Ncat});
+                // cout<<"x = "<<x<<' '<<"cat = "<<Ncat<<endl;
+            }
+        }
+        // cout<<endl;
+
+        if(node!=1 && f)ans++;
+        // cout<<"ans = "<<ans<<endl;
+    }
+    return ans;
+}
+void I_Am_Here() {
+    ll n,m;
+    cin>>n>>m;
+    for(int i=1 ; i<=n ; i++){
+        cin>>b[i];
+    }
+    for(int i=0 ; i<n-1 ; i++){
+        ll x,y;
+        cin>>x>>y;
+        a[x].push_back(y);
+        a[y].push_back(x);
+    }
+    cout<<bfs(1,m)<<endl;
+    
+    
+}
+
+int main() {    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t=1;
+    // cin>>t;
+    while (t--) {
+        I_Am_Here();
+    }
+    return 0;
+}
