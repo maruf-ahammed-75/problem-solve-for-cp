d42c5d36fd866b696b14b35e5817ce735cb95da9
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-01-06 16:37:23 +0600

    0/1 Knapsack

---
+++ Algorithm/DP/01Knapsack.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+ll N=101,Wight=1e5 +10;
+vector<ll>v(N);
+vector<ll>w(N);
+vector<vector<ll>>dp(N,vector<ll>(Wight));
+ll n,W;
+ll iterativeKnapsack(){
+    for(int i=0 ; i<=n ; i++){
+        for(int j=0 ; j<=W ;j++){
+            if(i==0 || j==0){
+                dp[i][j]=0;
+            }
+            else if (w[i - 1] <= j)
+                dp[i][j] = max(v[i - 1] + dp[i - 1][j - w[i - 1]], dp[i - 1][j]);
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
+    return dp[n][W];
+}
+void I_Am_Here() {
+    cin>>n>>W;
+    for(int i=0 ; i<n ; i++){
+        cin>>w[i]>>v[i];
+    }
+    cout<<iterativeKnapsack()<<endl;
+    
+}
+
+int main() {
+
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    for(int i=0 ;i<N ; i++)fill(dp[i].begin(),dp[i].end(),-1);
+
+    int t=1;
+    // cin>>t;
+    while (t--) {
+        I_Am_Here();
+    }
+    return 0;
+}
