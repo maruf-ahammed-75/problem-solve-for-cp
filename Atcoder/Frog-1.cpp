05e0d885c48db0288ea359a25472c9ea6697bedc
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-01-21 01:45:28 +0600

    at coder dp

--- Atcoder/Frog-1.cpp
+++ Atcoder/Frog-1.cpp
@@ -7,6 +7,15 @@
 using namespace std;
 const long long INF = 1e18;
 vector<ll>dp(1000000);
+ll cost(int n,vector<int>&a){
+    if(n==1)return 0;
+    if(dp[n]!=INF)return dp[n];
+    dp[n]=min(dp[n],cost(n-1,a)+abs(a[n]-a[n-1]));
+    if(n>1){
+        dp[n]=min(dp[n],cost(n-2 , a)+abs(a[n]-a[n-2]));
+    }
+    return dp[n];
+}
 void I_Am_Here() {
     ll n;
     cin>>n;
@@ -14,19 +23,20 @@ void I_Am_Here() {
     for(int i=1 ; i<=n ; i++){
         cin>>a[i];
     }
-    dp[0]=0;
-    dp[1]=0;
-    for(int i=2 ; i<=n ; i++){
-        dp[i]=min( dp[i-1]+ abs(a[i]-a[i-1]),dp[i]);
-        // cout<<dp[i-1]<<' '<<a[i]<<' '<<'-'<<a[i-1]<<' '<<abs(a[i]-a[i-1])<<' '<<dp[i]<<endl;
-        if(i!=2){
-            // cout<<"if= "<<' ';
-            dp[i]=min(dp[i-2]+abs(a[i]-a[i-2]),dp[i]);
-            // cout<<dp[i-2]<<' '<<a[i]<<' '<<'-'<<a[i-2]<<' '<<abs(a[i]-a[i-2])<<' '<<dp[i]<<endl;
-        }
-        // cout<<i<<' '<<dp[i]<<' '<<endl;
-    }
-    cout<<dp[n]<<endl;
+    cout<<cost(n,a)<<endl;
+    // dp[0]=0;
+    // dp[1]=0;
+    // for(int i=2 ; i<=n ; i++){
+    //     dp[i]=min( dp[i-1]+ abs(a[i]-a[i-1]),dp[i]);
+    //     // cout<<dp[i-1]<<' '<<a[i]<<' '<<'-'<<a[i-1]<<' '<<abs(a[i]-a[i-1])<<' '<<dp[i]<<endl;
+    //     if(i!=2){
+    //         // cout<<"if= "<<' ';
+    //         dp[i]=min(dp[i-2]+abs(a[i]-a[i-2]),dp[i]);
+    //         // cout<<dp[i-2]<<' '<<a[i]<<' '<<'-'<<a[i-2]<<' '<<abs(a[i]-a[i-2])<<' '<<dp[i]<<endl;
+    //     }
+    //     // cout<<i<<' '<<dp[i]<<' '<<endl;
+    // }
+    // cout<<dp[n]<<endl;
     
 }
 
