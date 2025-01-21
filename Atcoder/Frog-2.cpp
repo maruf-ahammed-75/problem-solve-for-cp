181e5ed9ec032c7edc16c8a06a5fcbc7a4fa998a
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-01-22 00:49:55 +0600

    Atcoder

--- Atcoder/Frog-2.cpp
+++ Atcoder/Frog-2.cpp
@@ -7,25 +7,39 @@
 using namespace std;
 const long long INF = 1e18;
 vector<ll>dp(1000000);
-// ll cost(int n,vector<int>&a){
-    
-// }
+int k;
+ll cost(int n,vector<int>&a){
+    if(n==1)return 0;
+    if(dp[n]!=INF)return dp[n];
+    ll minCost=INF;
+    for(int i=1 ; i<=k ; i++){
+        if(n-i>=1){
+            dp[n]=min(dp[n],cost(n-i,a)+abs(a[n]-a[n-i]));
+        }
+    }
+    // dp[n]=minCost;
+    return dp[n];
+}
 void I_Am_Here() {
     ll n;
     cin>>n;
-    ll k;
+    
     cin>>k;
     vector<ll>a(1+n);
     for(int i=1 ; i<=n ; i++){
         cin>>a[i];
     }
-    dp[0]=0;dp[1]=0;
-    for(int i=2 ; i<=n ; i++){
-        for(int j=1; j<=min(i-1,k) ; j++){
-            dp[i]=min(dp[i],(abs(a[i]-a[i-j])+ dp[i-j] ));
-        }
-    }
-    cout<<dp[n]<<endl;
+
+    ////interation
+    // dp[0]=0;dp[1]=0;
+    // for(int i=2 ; i<=n ; i++){
+    //     for(int j=1; j<=min(i-1,k) ; j++){
+    //         dp[i]=min(dp[i],(abs(a[i]-a[i-j])+ dp[i-j] ));
+    //     }
+    // }
+    // cout<<dp[n]<<endl;
+
+    cout<<cost(n,a)<<endl;
 }
 
 int32_t main() {
