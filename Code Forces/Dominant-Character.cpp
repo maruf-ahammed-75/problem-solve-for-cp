cafe08a83bc4c1b7ec181fbf6a3a65f7440d9c61
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-02-08 18:00:34 +0600

    CF

---
+++ Code Forces/Dominant-Character.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first 
+#define ss second
+#define endl '\n'
+using namespace std;
+void Solve(){
+    int n;
+    cin>>n;
+    string a;
+    cin>>a;
+    int ans = INT_MAX;
+    for(int i=0 ; i<n-1 ; i++){
+        if(a[i]=='a' &&  a[i+1]=='a'){
+            ans = min(ans,2LL);
+            break;
+        }
+    }
+    for(int i=0 ; i<n-2 ; i++){
+        if(a[i]=='a' && a[i+2]=='a' && a[i]!=a[i+1]){
+            ans = min(ans,3LL);
+            break;
+        }
+    }
+    for(int i=0 ; i<n-3 ; i++){
+        if(a[i]=='a' && a[i+3]=='a' && a[i+1]!=a[i+2]){
+            ans = min(ans,4LL);
+            break;
+        }
+    }
+    for(int i=0 ; i<n-6 ; i++){
+        if(a[i]=='a'&&a[i+1]==a[i+2]&&a[i+3]=='a'&&a[i+4]==a[i+5]&&a[i+6]=='a'){
+            ans = min(ans,7LL);
+            break;
+        }
+    }
+    if(ans==INT_MAX){
+        cout<<-1<<endl;
+    }
+    else cout<<ans<<endl;
+
+}
+
+int32_t main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int ts = 1;
+    cin >> ts;
+    while (ts--)
+    {
+        Solve();
+    }
+
+    return 0;
+}
