d5b3bb1021798916c95e387d25fe9feba89c05f8
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-03-05 01:36:35 +0600

    CF

--- Code Forces/SlavicG-Favorite-Problem.cpp
+++ Code Forces/SlavicG-Favorite-Problem.cpp
@@ -7,17 +7,43 @@
 #define Y cout<<"YES\n"
 #define N cout<<"NO\n"
 using namespace std;
-vector<pair<int,int>>adj[1e6];
+vector<pair<int,int>>adj[1000000+100];
+int n,a,b;
+map<int,int>m;
+bool ok;
+void dfs0(int cur,int par,int val){
+    if(cur == b)return;
+    m[val]=1;
+    for(auto i:adj[cur]){
+        if(i.ff==par)continue;
+        dfs0(i.ff,cur,i.ss^val);
+    }
+}
+void dfs1(int cur,int par,int val){
+    if(m[val] && cur != b){
+        ok = 1;
+    }
+    for(auto i:adj[cur]){
+        if(i.ff==par)continue;
+        dfs1(i.ff,cur,i.ss^val);
+    }
+}
 void I_Am_Here() {
-    for(int i=0 ; i<=1e6 ; i++)adj[i].clear();
-    int n,a,b;
+    
+    m.clear();
     cin>>n>>a>>b;
-    for(int i=1 ; i<=n ; i++){
+    for(int i=0 ; i<=n ; i++)adj[i].clear();
+    for(int i=1 ; i<=n-1 ; i++){
         int u,v,w;
         cin>>u>>v>>w;
         adj[u].push_back({v,w});
         adj[v].push_back({u,w});
     }
+    ok = 0;
+    dfs0(a,-1,0);
+    dfs1(b,-1,0);
+    if(ok)cout<<"YES\n";
+    else cout<<"NO\n";
 
 }
 
@@ -33,6 +59,6 @@ int32_t main() {
         I_Am_Here();
     }
         
-    
+
     return 0;
 }
