cd769f98ed38b0c3de7f86e6222896429bc654de
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-03-04 20:38:58 +0600

    PDF

---
+++ Code Forces/SlavicG-Favorite-Problem.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(),a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+vector<pair<int,int>>adj[1e6];
+void I_Am_Here() {
+    for(int i=0 ; i<=1e6 ; i++)adj[i].clear();
+    int n,a,b;
+    cin>>n>>a>>b;
+    for(int i=1 ; i<=n ; i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
+    }
+
+}
+
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t=1;
+    cin>>t;
+    for(int T=1 ; T<=t ; T++){
+        I_Am_Here();
+    }
+        
+    
+    return 0;
+}
