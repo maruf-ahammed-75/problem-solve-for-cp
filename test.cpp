c5751bdfc5d47e935035b3fe383f136a70a614e4
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-09 21:25:40 +0600

    Structure

--- test.cpp
+++ test.cpp
@@ -1,35 +1,27 @@
+//structure
 #include <bits/stdc++.h>
-#define ll long long int
+#define ll long long
+#define int long long
 #define ff first
 #define ss second
-#define full(a) a.begin(),a.end()
+#define full(a) a.begin(), a.end()
 #define Y cout<<"YES\n"
 #define N cout<<"NO\n"
 using namespace std;
 void I_Am_Here() {
-    int n,a,b;
-    string s;
-    cin>>n>>a>>b>>s;
-    ll ans=INT_MAX;
-
-    for(int k=1 ; k<=n ; k++){
-        ll sum=0;
-        s.erase(k-1,1);
-        cout<<s<<endl;
-        // for(int i=0 ; i<n ; i++){
-
-        // }
-    }
+    
 }
-
-int main() {
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
     
     int t=1;
-    // cin>>t;
-    while (t--) {
+    cin>>t;
+    for(int T=1 ; T<=t ; T++){
         I_Am_Here();
     }
+
     return 0;
 }
