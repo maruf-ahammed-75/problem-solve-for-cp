609ce71d6c1b189f3e80890d670469b184ba971e
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-16 00:06:46 +0600

    CF Binary search

---
+++ Code Forces/Kamilka-and-the-Sheep.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+void I_Am_Here() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(auto &i:a)cin>>i;
+    sort(full(a));
+    cout<<a[n-1]-a[0]<<"\n";
+}
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t=1;
+    cin>>t;
+    for(int T=1 ; T<=t ; T++){
+        I_Am_Here();
+    }
+
+    return 0;
+}
