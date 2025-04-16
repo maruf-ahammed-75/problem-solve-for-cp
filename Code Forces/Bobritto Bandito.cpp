c606fe0b56b5fa292ab446703e7212ce291f235f
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-16 23:14:04 +0600

    CF

---
+++ Code Forces/Bobritto Bandito.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+void I_Am_Here() {
+    int n,m,l,r;
+    cin>>n>>m>>l>>r;
+
+    if(r>=m)cout<<0<<' '<<m<<endl;
+    else{
+        cout<<r-m<<' '<<r<<endl;
+    }
+}
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t=1;
+    cin>>t;
+    for(int T=1 ; T<=t ; T++){
+        I_Am_Here();
+    }
+
+    return 0;
+}
