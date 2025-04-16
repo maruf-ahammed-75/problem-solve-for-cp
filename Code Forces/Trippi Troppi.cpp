2febc3e8005ce22b86efe32528cf083bd1b96363
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-16 23:12:25 +0600

    CF

---
+++ Code Forces/Trippi Troppi.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+void I_Am_Here() {
+    string a,b,c;
+    cin>>a>>b>>c;
+    cout<<a[0]<<b[0]<<c[0]<<endl;
+}
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t=1;
+    cin>>t;
+    for(int T=1 ; T<=t ; T++){
+        I_Am_Here();
+    }
+
+    return 0;
+}
