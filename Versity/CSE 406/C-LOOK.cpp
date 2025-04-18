256d8d20d815d065fdb4b940a81908ae0d7d60fc
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-18 20:05:06 +0600

    C - SCAN

---
+++ Versity/CSE 406/C-LOOK.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;//vector size n
+    vector<int> a(n);
+    vector<int> left, right;
+   
+    int head;
+    cin>> head;//initial head position
+
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];//input vector elements
+        if(a[i] < head) {
+            left.push_back(a[i]);//store elements less than head in left vector
+        } else {
+            right.push_back(a[i]);//store elements greater than or equal to head in right vector
+        }
+    }
+    
+    
+    sort(left.begin(), left.end());//sort left vector in descending order
+    sort(right.begin(), right.end());//sort right vector in ascending order
+    
+    cout << "Path: " << head;//print initial head position
+    
+    for(int i=0 ; i<right.size(); i++){
+        head = right[i];//update head position
+        cout << " -> " << head;//print path
+    }
+    for(int i=0 ; i<left.size(); i++){
+        head = left[i];//update head position
+        cout << " -> " << head;//print path
+    }
+    cout<<endl;
+
+    return 0;
+}
