ab45a62741aac431c8150ab7ad0f82552548d249
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-22 00:44:47 +0600

    CF

---
+++ Code Forces/Elections.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+
+void I_Am_Here(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int x = max(a,b);
+    x = max(x,c);
+    int ans =0;
+    if(x==a)ans++;
+    if(x==b)ans++;
+    if(x==c)ans++;
+    if(ans==1){
+        a = x-a;
+        b = x-b;
+        c = x-c;    
+        if(a==0){
+            cout<<0<<' ';
+        }
+        else{
+            cout<<a+1<<' ';
+        }
+        if(b==0){
+            cout<<0<<' ';
+        }
+        else{
+            cout<<b+1<<' ';
+        }
+        if(c==0){
+            cout<<0<<' ';
+        }
+        else{
+            cout<<c+1<<' ';
+        }
+    }
+    else{
+        a = x-a;
+        b = x-b;
+        c = x-c;
+        cout<<a+1<<' '<< b+1 <<' ' << c+1;
+    }
+    cout<<endl;
+}
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t=1;
+    cin>>t;
+    for(int T=1 ; T<=t ; T++){
+        I_Am_Here();
+    }
+
+    return 0;
+}
