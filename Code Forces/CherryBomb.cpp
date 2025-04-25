caaafe7918988352262d0171f385cbafd4c0480b
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-25 18:53:58 +0600

    CF

---
+++ Code Forces/CherryBomb.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+
+void I_Am_Here() {
+    int n, m;
+    cin >> n >> m;
+    for(int i=0 ; i<n ; i++){
+        if(i==m)continue;
+        cout<<i<<" ";
+    }
+    if(n==m){
+        return;
+    }
+    cout<<m<<endl;
+}
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t=1;
+    cin>>t;
+    for(int T=1 ; T<=t ; T++){
+        I_Am_Here();
+    }
+
+    return 0;
+}
