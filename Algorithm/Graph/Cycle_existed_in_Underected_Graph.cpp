4b26920a84409ec1694dca2a1bbaa6efd9604863
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-04-27 23:30:54 +0600

    in a undricted graph a cycle Existed or not

---
+++ Algorithm/Graph/Cycle_existed_in_Underected_Graph.cpp
@@ -0,0 +1,75 @@
+//undricted cycle
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+vector<vector<int>>a(1e6,vector<int>());
+vector<int>vis(1e5,0);
+vector<int>level(1e5,0);
+// void bfs(int s){
+//     vis[s]=1;
+//     level[s]=1;
+//     queue<int>q;
+//     q.push(s);
+//     while(q.size()){
+//         int cur = q.front();
+//         q.pop();
+//         for(auto i:a[cur]){
+           
+//             if(!vis[i]){
+//                 level[i]=level[cur]+1;
+//                 vis[i]=1;
+//                 q.push(i);
+//             }
+            
+//         }
+//     }
+// }
+vector<int>par(1e6,0);
+void dfs(int s){
+    vis[s]=1;
+    for(auto i:a[s]){
+        if(!vis[i]){
+            level[i]=1+level[s];
+            par[i] = s;
+            dfs(i);
+        }
+        else if(par[s]!=i){
+            cout<<"Cycle = "<<i<<endl;
+        }
+    }
+}
+void I_Am_Here() {
+    int n;
+    cin>>n;
+    vector<int>root;
+    for(int i=1,u ;i<=n; i++){
+        int v;
+        cin>>v>>u;
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+    // for(auto i:root){
+    //     if(!vis[i]){
+    //         level[i]=1;
+    //         dfs(i);
+    //     }
+    // }
+    dfs(1);
+    
+    // for(int i=1 ; i<=n ; i++)cout<<level[i]<<' ';
+    // cout<<*max_element(full(level))<<endl;
+}
+int32_t main() {
+    int t=1;
+    // cin>>t;
+    for(int T=1 ; T<=t ; T++){
+        I_Am_Here();
+    }
+    return 0;
+}
