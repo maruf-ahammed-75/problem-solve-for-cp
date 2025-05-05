ca54ff475209841c01186cf5c5aaf262568bc0a3
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-05-05 22:21:36 +0600

    CF

--- Code Forces/1.cpp
+++ Code Forces/1.cpp
@@ -1,62 +1,27 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 100005;
-vector<int> adj[MAXN];  // Adjacency list
-int cats[MAXN];         // Whether a node contains a cat (1 or 0)
-bool visited[MAXN];     // Visited array
-int n, m, validLeaves = 0;
-
-void dfs(int node, int consecutiveCats) {
-    visited[node] = true;
-
-    if (cats[node] == 1) {
-        consecutiveCats++;
-    } else {
-        consecutiveCats = 0;
-    }
-
-    if (consecutiveCats > m) {
-        return;
-    }
-
-    bool isLeaf = true;  // Assume it's a leaf
-
-    for (int neighbor : adj[node]) {
-        if (!visited[neighbor]) {
-            isLeaf = false;  // Not a leaf if it has unvisited children
-            dfs(neighbor, consecutiveCats);
-        }
-    }
-
-    // If it's a leaf and satisfies the condition, count it as valid
-    if (isLeaf) {
-        validLeaves++;
-    }
-}
-
 int main() {
-    cin >> n >> m;
-    
-    // Input cat information
-    for (int i = 1; i <= n; ++i) {
-        cin >> cats[i];
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t; cin >> t;
+    while (t--) {
+        int n;
+        long long k;
+        cin >> n >> k;
+        vector<long long> a(n);
+        for (auto &x : a) cin >> x;
+
+        long long minA = *min_element(a.begin(), a.end());
+        long long moves = 0;
+        for (int i = 0; i < n; ++i) {
+            if (a[i] <= minA + k)
+                moves += a[i] - minA;
+        }
+
+        cout << (moves % 2 ? "Tom\n" : "Jerry\n");
     }
-
-    // Input tree edges
-    for (int i = 0; i < n - 1; ++i) {
-        int x, y;
-        cin >> x >> y;
-        adj[x].push_back(y);
-        adj[y].push_back(x);
-    }
-
-    // Start DFS from root (vertex 1) with initial consecutive cat count as 0
-    dfs(1, 0);
-
-    // Output the number of valid leaves
-    cout << validLeaves << endl;
 
     return 0;
 }
--- Code Forces/2.cpp
+++ Code Forces/2.cpp
@@ -1,51 +1,165 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
 using namespace std;
-
-const int N = int(2e5) + 99;
-const int INF = int(1e9) + 99;
-
-int tc;
-string s, t;
-int nxt[N][26];
-
-int main() {
-    cin >> tc;
-    while(tc--){
-        cin >> s >> t;
-        for(int i = 0; i < s.size() + 5; ++i)
-            for(int j = 0; j < 26; ++j)
-                nxt[i][j] = -1;
-    	
-        for(int i = int(s.size()) - 1; i >= 0; --i){
-            for(int j = 0; j < 26; ++j)
-                nxt[i][j] = nxt[i + 1][j];
-            nxt[i][s[i] - 'a'] = i;
-        }
-        for(int i=0 ; i<s.size() ; i++){
-            for(int j = 0; j < 26; ++j)cout<<nxt[i][j]<<' ';
-            cout<<endl;
-        }    
-    
-        int res = 1, pos = 0;
-        for(int i = 0; i < t.size(); ++i){
-            if(pos == s.size()){
-                pos = 0;
-                ++res;
-            }
-            if(nxt[pos][t[i] - 'a'] == INF){
-                pos = 0; 
-                ++res;
-    		}
-    		if(nxt[pos][t[i] - 'a'] == INF && pos == 0){
-                res = INF;
+void I_Am_Here() {
+    int n;
+    int k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    vector<int> a(n);
+    vector<int> b(n);
+    for (int i = 0; i < n; i++) {
+        cin>>  a[i];
+        b[i] = a[i];
+    }
+    sort(full(b));
+    if(b[0]==b[n-1] && b[0]==0){
+        cout<<"Yes"<<endl;
+        for(int i=0 ; i<n ; i++){
+            if(s[i]=='0'){
+                a[i] = k;
                 break;
-            }    
-            pos = nxt[pos][t[i] - 'a'] + 1;
-            
+            }
         }
+        for(int i=0 ; i<n ; i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+        
+    int max_sum = INT_MIN;
+    int current_sum = 0;
+    int in = 0;
+    for (int i = 0; i < a.size(); ++i) {
+        if(s[i]=='0')continue;
+        current_sum = max(a[i], current_sum + a[i]);
+        if(current_sum>max_sum) {
+            max_sum = current_sum;
+            in = i;
+        }
+    }
+    // cout<<max_sum<<" "<<in<<endl;
+    bool flag = false;
+    int sum=0;
+    if(max_sum >k){
+        int i=in;
+        while(i>=0){
+            sum+=a[i];
+            if(sum>k){
+                cout<<"No"<<endl;
+                return;
+            }
+            if(s[i]=='0'){
+                flag=true;
+                a[i] = -(max_sum-k);
+                break;
+            }
+            i--;
+        }
+    }
+    else{
+        ll need = k - max_sum;
+        ll sum=0;
+        for(int i=in ; i>=0 ; i--){
+            // if(a[i]<0){
+            //     need+=abs(a[i]);
+            // }
+            if(s[i]=='0'){
+                a[i] = need;
+                flag=true;
+                break;
+            }
+            sum+=a[i];
+            if(sum==max_sum){
+                for(int j=i-1 ; j>=0 ; j--){
+                    if(a[j]<0){
+                        need+=abs(a[j]);
+                    }
+                    if(s[j]=='0'){
+                        a[j] = need;
+                        flag=true;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        if(!flag){
+            for(int i=in+1 ; i<n ; i++){
+                if(a[i]<0){
+                    need+=abs(a[i]);
+                }
+                if(s[i]=='0'){
+                    flag=true;
+                    a[i] = need;
+                    break;
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+     max_sum = INT_MIN;
+     current_sum = 0;
+     in = 0;
+    for (int i = 0; i < a.size(); ++i) {
+        
+        current_sum = max(a[i], current_sum + a[i]);
+        if(current_sum>max_sum) {
+            max_sum = current_sum;
+            in = i;
+        }
+    }
+    // cout<<max_sum<<" "<<in<<endl;
+     flag = false;
+    if(max_sum >k){
+        for(int i=in+1 ; i<n ; i++){
+            if(s[i]=='0' && a[i]==0){
+                flag=true;
+                a[i] = -(max_sum-k);
+                break;
+            }
+        }
+    }
+
+
+
+
+    if(flag){
+        cout<<"Yes"<<endl;
+        for(int i=0 ; i<n ; i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+        
+}
+
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
     
-        if(res >= INF) cout << -1 << endl;
-        else cout << res << endl;
+    int t=1;
+    cin>>t;
+    for(int T=1 ; T<=t ; T++){
+        I_Am_Here();
     }
     return 0;
 }
