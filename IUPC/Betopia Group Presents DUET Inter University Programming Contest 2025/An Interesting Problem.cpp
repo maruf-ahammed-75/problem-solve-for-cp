11e40418508632b3a371a3556e787e05696be7c8
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-05-13 23:49:23 +0600

    IUPC

---
+++ IUPC/Betopia Group Presents DUET Inter University Programming Contest 2025/An Interesting Problem.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+
+void I_Am_Here() {
+    int n;
+    cin>> n;
+    string s;
+    cin>> s;
+    map<char,int> m;
+    for(int i=0 ; i<n ; i++){
+        m[s[i]]++;
+        if(m[s[i]]>1){
+            cout<<"NO"<<endl;
+            return;
+        }
+    }
+    cout<<"YES"<<endl;
+}
+
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t=1;
+    cin>>t;
+    for(int T=1 ; T<=t ; T++){
+        I_Am_Here();
+    }
+
+    return 0;
+}
