33f5caa1f381edd8808fe3635e983de0235d758b
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-07-23 18:33:57 +0600

    CF

---
+++ Code Forces/Only One Digit.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+void I_Am_Here() {
+    int n;
+    cin>>n;
+    int mi = INT_MAX;
+    while(n){
+        int x = n%10;
+        n/=10;
+        mi = min(mi,x);
+    }
+    cout<<mi<<endl;
+}
+
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    cin >> t;
+    for (int T = 1; T <= t; T++) {
+        I_Am_Here();
+    }
+    return 0;
+}
