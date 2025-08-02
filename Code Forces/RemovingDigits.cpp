a3a291c9516e1bcc11d0de3018b5b06efe011842
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-08-02 19:00:12 +0600

    DP

--- Code Forces/RemovingDigits.cpp
+++ Code Forces/RemovingDigits.cpp
@@ -9,25 +9,38 @@
 using namespace std;
 
 //recursion
-int steps(int num){
-    if(num==0) return 0;
-    if(num<0)return INT_MAX;
+// int steps(int num){
+//     if(num==0) return 0;
+//     if(num<0)return INT_MAX;
 
 
-    int temp = num;
-    int ans=INT_MAX;
-    while(temp){
-        int x = temp%10;
-        if(x)ans = min(ans,1+steps(num-x));
-        temp/=10;
-    }
-    return ans;
+//     int temp = num;
+//     int ans=INT_MAX;
+//     while(temp){
+//         int x = temp%10;
+//         if(x)ans = min(ans,1+steps(num-x));
+//         temp/=10;
+//     }
+//     return ans;
 
-}
+// }
 void I_Am_Here() {
     int n;
     cin>>n;
-    cout<<steps(n)<<endl;
+    // cout<<steps(n)<<endl;
+    vector<int>steps(n+1,INT_MAX);
+
+    steps[0]=0;
+    for(int num=0 ; num<=n ; num++){
+        int temp = num;
+        while(temp){
+            int x = temp%10;
+            if(x && num-x>=0) steps[num] = min(steps[num],1+steps[num-x]);
+            temp/=10;
+        }
+    }
+    cout<<steps[n]<<endl;
+
 }
 
 int32_t main() {
