4b3319bac621d42d94b2f32809a6f7d7c168bdc6
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-08-03 23:38:19 +0600

    Recursion

---
+++ Code Forces/Edit-Distance.cpp
@@ -0,0 +1,53 @@
+
+ 
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+int n,m;
+string a,b;
+
+int edit(int i,int j){
+
+    if(i==n)return m-j;
+    if(j==m)return n-i;
+
+
+    int ans=INT_MAX;
+
+    if(a[i]==b[j])ans = min(ans,edit(i+1,j+1)); 
+    else{
+        ans = min(ans,edit(i+1,j)+1);// delete s1[i]
+        ans = min(ans,edit(i,j+1)+1);// insert s2[j] to s1
+        ans = min(ans,edit(i+1,j+1)+1);//replace
+    }
+
+    return ans;
+}
+void I_Am_Here() {
+    
+    cin>>a>>b;
+    n = a.size(),m=b.size();
+
+    cout<<edit(0,0)<<endl;
+ 
+}
+ 
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+    int t = 1;
+    // cin >> t;
+    for (int T = 1; T <= t; T++) {
+        I_Am_Here();
+    }
+    return 0;
+}
