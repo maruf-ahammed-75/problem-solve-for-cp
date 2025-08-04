fe77a1668b111f48ec5490227d014b33d8c20f58
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-08-04 23:32:37 +0600

    DP

--- Code Forces/Edit-Distance.cpp
+++ Code Forces/Edit-Distance.cpp
@@ -12,30 +12,47 @@ using namespace std;
 int n,m;
 string a,b;
 
-int edit(int i,int j){
+// int edit(int i,int j){
 
-    if(i==n)return m-j;
-    if(j==m)return n-i;
+//     if(i==n)return m-j;
+//     if(j==m)return n-i;
 
 
-    int ans=INT_MAX;
+//     int ans=INT_MAX;
 
-    if(a[i]==b[j])ans = min(ans,edit(i+1,j+1)); 
-    else{
-        ans = min(ans,edit(i+1,j)+1);// delete s1[i]
-        ans = min(ans,edit(i,j+1)+1);// insert s2[j] to s1
-        ans = min(ans,edit(i+1,j+1)+1);//replace
-    }
+//     if(a[i]==b[j])ans = min(ans,edit(i+1,j+1)); 
+//     else{
+//         ans = min(ans,edit(i+1,j)+1);// delete s1[i]
+//         ans = min(ans,edit(i,j+1)+1);// insert s2[j] to s1
+//         ans = min(ans,edit(i+1,j+1)+1);//replace
+//     }
 
-    return ans;
-}
+//     return ans;
+// }
 void I_Am_Here() {
     
     cin>>a>>b;
     n = a.size(),m=b.size();
 
-    cout<<edit(0,0)<<endl;
- 
+    // cout<<edit(0,0)<<endl;
+
+    vector<vector<int>>edit(n+1,vector<int>(m+1,INT_MAX));
+
+    if(a[i]==b[j]){
+        edit[i][j]=min(edit[i][j],edit[i+1][j+1]);
+    }
+    else{
+        edit[i][j]=min(edit[i][j],edit[i][j+1]+1);
+        edit[i][j]=min(edit[i][j],edit[i+1][j]+1);
+        edit[i][j]=min(edit[i][j],edit[i+1][j+1]+1);
+    }
+    if(i==n-1 && j!=m-1){
+
+    }
+    if(j==m-1 && i!=n-1){
+
+    }
+
 }
  
 int32_t main() {
---
+++ Code Forces/Rectangle-Cutting.cpp
@@ -0,0 +1,54 @@
+
+ 
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+int n,m;
+vector<vector<int>>dp;
+int Rectangle(int a,int b){
+
+    if(a==b)return 0;
+    // if(a<=0 || b<=0)return INT_MAX;
+    if(dp[a][b]!=INT_MAX)return dp[a][b];
+
+    
+
+
+    for(int i=1 ; i<a ; i++){
+        dp[a][b] = min(dp[a][b],1 + Rectangle(a-i,b) + Rectangle(i,b));
+
+    }
+    for(int i=1 ; i<b ; i++){
+        dp[a][b] = min(dp[a][b],1 + Rectangle(a,b-i) + Rectangle(a,i));
+    }
+
+    return dp[a][b];
+    
+}
+
+void I_Am_Here() {
+    cin>>n>>m;
+    dp = vector<vector<int>>(n+1,vector<int>(m+1,INT_MAX));
+    cout<<Rectangle(n,m)<<endl;
+
+}
+ 
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+    int t = 1;
+    // cin >> t;
+    for (int T = 1; T <= t; T++) {
+        I_Am_Here();
+    }
+    return 0;
+}
