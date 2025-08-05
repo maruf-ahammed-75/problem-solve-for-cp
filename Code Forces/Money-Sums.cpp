0aa565f888c27551c128e2b7e658cdd6e08aaa97
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-08-05 17:57:14 +0600

    DP

---
+++ Code Forces/Money-Sums.cpp
@@ -0,0 +1,56 @@
+
+ 
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+int n;
+vector<int>a;
+set<int>s;
+vector<vector<int>>dp;
+void sums(int index,int sum) {
+    if(dp[index][sum])return;
+
+    if(sum)s.insert(sum);
+    
+    if(index==n)return;
+    dp[index][sum] = 1;
+    sums(index + 1, sum + a[index]);
+    sums(index + 1, sum);
+}
+void I_Am_Here() {
+    cin>>n;
+    a = vector<int>(n);
+    int all =0;
+    for(int i=0 ; i<n ; i++){
+        cin>>a[i];
+        all += a[i];
+    }
+    dp = vector<vector<int>>(n + 1, vector<int>(all + 1, 0));
+    sort(full(a));
+    sums(0,0);
+    cout<<s.size()<<endl;
+    for(auto it : s) {
+        cout<<it<<" ";
+    }
+    cout<<endl;
+}
+ 
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+ 
+    int t = 1;
+    // cin >> t;
+    for (int T = 1; T <= t; T++) {
+        I_Am_Here();
+    }
+    return 0;
+}
--- Code Forces/Rectangle-Cutting.cpp
+++ Code Forces/Rectangle-Cutting.cpp
@@ -12,13 +12,9 @@ using namespace std;
 int n,m;
 vector<vector<int>>dp;
 int Rectangle(int a,int b){
-
     if(a==b)return 0;
     // if(a<=0 || b<=0)return INT_MAX;
     if(dp[a][b]!=INT_MAX)return dp[a][b];
-
-    
-
 
     for(int i=1 ; i<a ; i++){
         dp[a][b] = min(dp[a][b],1 + Rectangle(a-i,b) + Rectangle(i,b));
@@ -27,16 +23,14 @@ int Rectangle(int a,int b){
     for(int i=1 ; i<b ; i++){
         dp[a][b] = min(dp[a][b],1 + Rectangle(a,b-i) + Rectangle(a,i));
     }
-
     return dp[a][b];
-    
 }
 
 void I_Am_Here() {
     cin>>n>>m;
     dp = vector<vector<int>>(n+1,vector<int>(m+1,INT_MAX));
-    cout<<Rectangle(n,m)<<endl;
-    vector<int>c;
+    // cout<<Rectangle(n,m)<<endl;
+    
 }
  
 int32_t main() {
