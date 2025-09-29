04b02ab1b0724e78212ba34822d716d377e06ded
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-09-29 22:46:04 +0600

    Segment tree

--- Code Forces/List-Removals.cpp
+++ Code Forces/List-Removals.cpp
@@ -5,33 +5,37 @@
 #define full(a) a.begin(), a.end()
 #define Y cout<<"YES\n"
 #define N cout<<"NO\n"
-#include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-using namespace __gnu_pbds;
+// using namespace __gnu_pbds;
 
-typedef tree<int, null_type, less<int>, rb_tree_tag,
-             tree_order_statistics_node_update> indexed_set;
+// typedef tree<int, null_type, less<int>, rb_tree_tag,
+//              tree_order_statistics_node_update> indexed_set;
 
 #define int long long
 
 void I_Am_Here() {
-    int n;
-    cin>>n;
-    std::vector<int>a(n) ;
-    indexed_set s;
-    for(int i= 0; i<n ; i++){
-        cin>>a[i];
-        s.insert(i+1);
-    }
+    // indexing set
+    // int n;
+    // cin>>n;
+    // std::vector<int>a(n) ;
+    // indexed_set s;
+    // for(int i= 0; i<n ; i++){
+    //     cin>>a[i];
+    //     s.insert(i+1);
+    // }
     
-    for(int i=0 ; i<n ; i++){
-        int x;
-        cin>>x;
-        x--;
-        int y = *s.find_by_order(x);
-        cout<<a[--y]<<' ';
-        s.erase(y+1);
-    }
+    // for(int i=0 ; i<n ; i++){
+    //     int x;
+    //     cin>>x;
+    //     x--;
+    //     int y = *s.find_by_order(x);
+    //     cout<<a[--y]<<' ';
+    //     s.erase(y+1);
+    // }
+
+
+    
 }
 
 int32_t main() {
