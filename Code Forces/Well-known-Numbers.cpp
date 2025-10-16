deda0a07069f3e4c732dbaed1ea062837a92722e
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-10-16 08:11:29 +0600

    fibbonacche

--- Code Forces/Well-known-Numbers.cpp
+++ Code Forces/Well-known-Numbers.cpp
@@ -18,7 +18,54 @@ int cal(int k,int n){
     return ans;
 }
 void I_Am_Here() {
+    int s,k;
+    cin>>s>>k;
+    vector<int>a;
+    a.push_back(1);
+    a.push_back(1);
+    int sum=2;
+    int temp = k-1;
+    while(temp-- && sum <= 1000000000){
+        a.push_back(sum);
+        sum+=a[a.size()-1];
+    }
+    // for(int i=0 ; i<a.size() ; i++){
+    //     cout<<a[i]<<" ";
+    // }
+    // cout<<"\n";
+    // cout<<sum<<"\n";
+    int j=0;
+    while(1000000000*1LL >= sum){
+        int x = sum-a[j];
+        a.push_back(x);
+        sum += x-a[j];
+        j++;
+        // cout<<x<<' ';
+    }
+    // cout<<"\n";
+
+
+    // for(int i = 0 ; i<a.size() ; i++){
+    //     cout<<a[i]<<" ";
+    // }
+    // cout<<"\n";
+
+
+    vector<int>v;
     
+    a[0]=0;
+    for(int i = a.size()-1 ; i>=0 ; i--){
+        if(s>=a[i]){
+            v.push_back(a[i]);
+            s-=a[i];
+        }
+    }
+    reverse(full(v));
+    cout<<v.size()<<"\n";
+    for(int i = 0 ; i<v.size() ; i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<"\n";
 }
 
 int32_t main() {
