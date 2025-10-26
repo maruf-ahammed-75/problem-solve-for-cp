a033756dedde7f34486351c25419fd4258e6fc95
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-10-26 23:12:21 +0600

    Order set

--- Code Forces/Vitamins.cpp
+++ Code Forces/Vitamins.cpp
@@ -7,75 +7,117 @@
 #define Y cout<<"YES\n"
 #define N cout<<"NO\n"
 using namespace std;
+vector<int> cost;
+vector<string> s;
+int n;
+int dp[1000+10][2][2][2];
+int cal(int id,int sum,bool a,bool b,bool c){
+    
+    if(a&&b&&c){
+        return sum;
+    }
+    if(id == n)return INT_MAX;
 
+    int ans = INT_MAX;
+    //skip
+    ans = min(ans,cal(id+1,sum,a,b,c));
+
+    //take
+    bool x=0,y=0,z=0;
+    for(auto i: s[id]){
+        if(i=='A')x=1;
+        if(i=='B')y=1;
+        if(i=='C')z=1;
+    }
+
+    ans = min(ans,cal(id+1,sum+cost[id],a|x,b|y,c|z));
+
+    return ans;
+}
 
 void I_Am_Here() {
-    int n;
     cin>>n;
-    vector<int> a,b,c,ab,ac,bc,abc;
+    cost = vector<int>(n);
+    s = vector<string>(n);
     for(int i=0 ; i<n ; i++){
-        int x;
-        string s;
-        cin>>x>>s;
-        sort(full(s));
-        if(s=="A"){
-            a.push_back(x);
-        }
-        else if(s=="B"){
-            b.push_back(x);
-        }
-        else if(s=="C"){
-            c.push_back(x);
-        }
-        else if(s=="AB"){
-            ab.push_back(x);
-        }
-        else if(s=="BC"){
-            bc.push_back(x);
-        }
-        else if(s=="AC"){
-            ac.push_back(x);
-        }
-        else if(s=="ABC"){
-            abc.push_back(x);
-        }
+        cin>>cost[i]>>s[i];
     }
-    sort(full(a));
-    sort(full(b));
-    sort(full(c));
-    sort(full(ab));
-    sort(full(ac));
-    sort(full(bc));
-    sort(full(abc));
-    int ans = INT_MAX;
-    if(a.size() && b.size() && c.size()){
-        ans = min(ans , a[0] + b[0] + c[0]);
+    int x = cal(0,0,0,0,0);
+    if(x == INT_MAX){
+        cout<<-1<<"\n";
+        return;
     }
-    if(abc.size()){
-        ans = min(ans , abc[0]);
-    }
-    if(ab.size() && c.size()){
-        ans = min(ans , ab[0] + c[0]);
-    }
-    if(bc.size() && a.size()){
-        ans = min(ans , bc[0] + a[0]);
-    }
-    if(ac.size() && b.size()){
-        ans = min(ans , ac[0] + b[0]);
-    }
-    if(ac.size() && bc.size()){
-        ans = min(ans , ac[0] + bc[0]);
-    }
-    if(ab.size() && ac.size()){
-        ans = min(ans , ab[0] + ac[0]);
-    }
-    if(ab.size() && bc.size()){
-        ans = min(ans , ab[0] + bc[0]);
-    }
-    if(ans == INT_MAX){
-        ans = -1;
-    }
-    cout<<ans<<"\n";
+    cout<<x<<"\n";
+
+
+
+
+
+
+    // vector<int> a,b,c,ab,ac,bc,abc;
+    // for(int i=0 ; i<n ; i++){
+    //     int x;
+    //     string s;
+    //     cin>>x>>s;
+    //     sort(full(s));
+    //     if(s=="A"){
+    //         a.push_back(x);
+    //     }
+    //     else if(s=="B"){
+    //         b.push_back(x);
+    //     }
+    //     else if(s=="C"){
+    //         c.push_back(x);
+    //     }
+    //     else if(s=="AB"){
+    //         ab.push_back(x);
+    //     }
+    //     else if(s=="BC"){
+    //         bc.push_back(x);
+    //     }
+    //     else if(s=="AC"){
+    //         ac.push_back(x);
+    //     }
+    //     else if(s=="ABC"){
+    //         abc.push_back(x);
+    //     }
+    // }
+    // sort(full(a));
+    // sort(full(b));
+    // sort(full(c));
+    // sort(full(ab));
+    // sort(full(ac));
+    // sort(full(bc));
+    // sort(full(abc));
+    // int ans = INT_MAX;
+    // if(a.size() && b.size() && c.size()){
+    //     ans = min(ans , a[0] + b[0] + c[0]);
+    // }
+    // if(abc.size()){
+    //     ans = min(ans , abc[0]);
+    // }
+    // if(ab.size() && c.size()){
+    //     ans = min(ans , ab[0] + c[0]);
+    // }
+    // if(bc.size() && a.size()){
+    //     ans = min(ans , bc[0] + a[0]);
+    // }
+    // if(ac.size() && b.size()){
+    //     ans = min(ans , ac[0] + b[0]);
+    // }
+    // if(ac.size() && bc.size()){
+    //     ans = min(ans , ac[0] + bc[0]);
+    // }
+    // if(ab.size() && ac.size()){
+    //     ans = min(ans , ab[0] + ac[0]);
+    // }
+    // if(ab.size() && bc.size()){
+    //     ans = min(ans , ab[0] + bc[0]);
+    // }
+    // if(ans == INT_MAX){
+    //     ans = -1;
+    // }
+    // cout<<ans<<"\n";
 
 }
 
