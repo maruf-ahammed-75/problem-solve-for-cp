86f76efffe83915e10c65959fbef3733d93ff414
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-10-27 18:09:24 +0600

    LIS

---
+++ Template/dp/longest increasing subsequence.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLIS(vector<int>& arr) {
+    vector<int> lis;
+    for(int x : arr) {
+        auto it = lower_bound(lis.begin(), lis.end(), x);
+        if(it == lis.end()) lis.push_back(x);
+        else *it = x;
+    }
+    return lis.size();
+}
+
+int main() {
+    vector<int> arr = {10, 22, 9, 33, 21, 50, 41, 60};
+    cout << lengthOfLIS(arr) << "\n";
+    return 0;
+}
