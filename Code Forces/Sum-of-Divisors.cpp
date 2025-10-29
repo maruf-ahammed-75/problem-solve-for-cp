bd8bf8f27c11786ce8d2df7df40a9b81615bfa10
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-10-29 22:05:17 +0600

    MATH formula

---
+++ Code Forces/Sum-of-Divisors.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+void I_Am_Here() {
+   int n;
+   cin>>n;
+   int ans =0 ;
+   for(int i=1 ; i*i<=n ; i++){
+        int x = n/i;//1 ACHE 5 BAR
+        int y = n/x;//5 ACHE 1 BAR
+        int total = (x*i)%MOD;
+        if(x!=y){
+            total += (y*x)%MOD;
+            total %= MOD;
+        }
+        ans += total;
+        ans %= MOD;
+   }
+   cout<<ans<<endl;
+
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    // cin >> t;
+    for (int T = 1; T <= t; T++) {
+        I_Am_Here();
+    }
+    return 0;
+}
