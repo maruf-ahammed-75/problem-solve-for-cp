d4ddf965f249230cc479c755eb2eabd73611f0bc
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-10-29 22:43:12 +0600

    math

--- Code Forces/Sum-of-Divisors.cpp
+++ Code Forces/Sum-of-Divisors.cpp
@@ -6,27 +6,32 @@
 #define full(a) a.begin(), a.end()
 #define Y cout<<"YES\n"
 #define N cout<<"NO\n"
+#define i128 __int128_t
 using namespace std;
 
-const int MOD = 1e9 + 7;
+
 
 void I_Am_Here() {
+    int MOD = 1e9 + 7;
    int n;
    cin>>n;
-   int ans =0 ;
-   for(int i=1 ; i*i<=n ; i++){
-        int x = n/i;//1 ACHE 5 BAR
-        int y = n/x;//5 ACHE 1 BAR
-        int total = (x*i)%MOD;
-        if(x!=y){
-            total += (y*x)%MOD;
-            total %= MOD;
-        }
-        ans += total;
-        ans %= MOD;
-   }
-   cout<<ans<<endl;
-
+   int i=1;
+    int ans=0;
+    i128 q;
+    while(i<=n){
+        q = n/i;
+        int nexti = n/q + 1;
+        i128 x = (nexti-1-i+1);
+        i128 y = (nexti-1+i);
+        i128 total = (x*y)/2;
+        total%=MOD;
+        total = (total*q)%MOD;
+        
+        ans+=(long long)total;
+        ans%=MOD;
+        i=nexti;
+    }
+    cout<<ans%MOD<<endl;
 }
 
 int32_t main() {
@@ -34,10 +39,10 @@ int32_t main() {
     cin.tie(nullptr);
 
 
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
 
     int t = 1;
     // cin >> t;
