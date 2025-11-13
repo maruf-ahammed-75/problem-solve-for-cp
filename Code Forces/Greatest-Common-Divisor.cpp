289ba64c8f58275e4a9fb3a69043eae8de04e4af
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-11-13 18:28:59 +0600

    CF

---
+++ Code Forces/Greatest-Common-Divisor.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define endl "\n"
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+
+using namespace std;
+
+void I_Am_Here() {
+    int n,m;
+    cin>>n>>m;
+    cout<<__gcd(n,m)<<endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    // cin >> t;
+    for (int T = 1; T <= t; T++) {
+        I_Am_Here();
+    }
+    return 0;
+}
