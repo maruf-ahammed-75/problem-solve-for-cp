82642a961b17975fcf0b965099baf3c942805871
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-11-18 09:06:09 +0600

    CF

--- Code Forces/Cyclic-Merging.cpp
+++ Code Forces/Cyclic-Merging.cpp
@@ -14,75 +14,13 @@ void I_Am_Here() {
     int n;
     cin>>n;
     vector<int>a(n);
-    vector<pair<int,int>>b(n);
-    for(int i =0 ; i<n ; i++){
-        cin>>a[i];
-        b[i] = {a[i],i};
-    }
-    sort(full(b));
-
-    for(int i=0 ; i<n ; i++){
-        cout<<b[i].ff<<" "<<b[i].ss<<endl;
-    }
-    cout<<endl;
-
+    for(int i=0;i<n;i++) cin>>a[i];
     int sum=0;
-
+    int mxval = max_element(full(a)) - a.begin();
     for(int i=0 ; i<n ; i++){
-        int id = b[i].ss;
-        cout<<"id = "<<id<< "     ";
-        if(id==0){
-            int x = max(a[id],a[id+1]);
-            int y = max(a[id],a[n-1]);
-            int z = min(x,y);
-            sum += z;
-            if(x<y){
-                cout<<"1 if"<<endl;    
-                a[id]=z;
-                a[id+1]=z;
-            }
-            else{
-                cout<<"1 else"<<endl; 
-                a[id]=z;
-                a[n-1]=z;
-            }
-        }
-        else if(id==n-1){
-              
-            int x = max(a[id],a[0]);
-            int y = max(a[id],a[id-1]);
-            int z = min(x,y);
-            sum += z;
-            if(x<y){
-                cout<<"2 if"<<endl;   
-                a[id]=z;
-                a[0]=z;
-            }
-            else{
-                cout<<"2 else"<<endl;
-                a[id]=z;
-                a[id-1]=z;
-            }
-        }
-        else{
-            int x = max(a[id],a[id-1]);
-            int y = max(a[id],a[id+1]);
-            int z = min(x,y);
-            sum += z;
-            if(x<y){
-                cout<<"3 if"<<endl;    
-                a[id]=z;
-                a[id-1]=z;
-            }
-            else{
-                cout<<"3 else"<<endl;
-                a[id]=z;
-                a[id+1]=z;
-            }
-        }
-        cout<<"sum = "<<sum<<endl;
+        sum+=max(a[i],a[(i+1)%n]);
     }
-    cout<<sum<<endl;
+    cout<<sum - a[mxval]<<endl;
 }
 
 int32_t main() {
