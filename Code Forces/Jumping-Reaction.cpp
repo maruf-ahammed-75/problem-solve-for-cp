bd110432e9865e46555f4fd9a867de29141044d5
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-11-19 21:35:21 +0600

    CF

--- Code Forces/Jumping-Reaction.cpp
+++ Code Forces/Jumping-Reaction.cpp
@@ -9,27 +9,40 @@
 #define N cout<<"NO\n"
 
 using namespace std;
-const int mod = 998244353;
+
+const int mod = 1e9 + 7 ;
+
+long long modpow(long long a, long long b, long long m = mod){
+    long long res = 1;
+    a %= m;
+    while(b > 0){
+        if(b & 1) res = (res * a) % m;
+        a = (a * a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+
 void I_Am_Here() {
     int n;
     int q;
     cin>>n>>q;
     vector<int>a(n),sum(n+1,0),squre(n+1,0);
+
     for(int i=0;i<n;i++){
         cin>>a[i];
-        sum[i+1]=sum[i]+a[i];
-        squre[i+1]=squre[i]+(a[i]*a[i]);
+        sum[i+1]=(sum[i]+a[i])%mod;
+        squre[i+1]=(squre[i]+(a[i]*a[i])%mod)%mod ;
     }
-    // for(int i=1 ; i<=n ; i++){
-    //     cout<<sum[i]<<" "<<squre[i]<<endl;
-    // }
 
     while(q--){
         int l,r;
         cin>>l>>r;
-        int x = sum[r]-sum[l-1];
-        int y = squre[r]-squre[l-1];
-        cout<<(x*x - y)/2<<endl;
+        int x = (sum[r]-sum[l-1] + mod)%mod;
+        int y = (squre[r]-squre[l-1] + mod) % mod;
+        int ans = ((x*x)%mod - y + mod)%mod;
+        ans = (ans * modpow(2,mod-2,mod))%mod;
+        cout<<ans<<endl;
     }
 }
 
