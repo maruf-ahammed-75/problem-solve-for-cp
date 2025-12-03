a53ab91d23b4e6461456b36c7e34e97473889b53
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-12-03 20:16:36 +0600

    lucas theorem

--- Code Forces/1.py
+++ Code Forces/1.py
@@ -1,5 +1,17 @@
-n=1558
+n=100
 fact = 1
 for i in range(1,n+1,1):
     fact = fact * i
 print(fact)
+
+fact2 = 1
+for i in range(1,26,1):
+    fact2 = fact2 * i
+print(fact2)
+
+fact3 = 1
+for i in range(1,76,1):
+    fact3 = fact3 * i
+print(fact3)
+
+print(((fact)//(fact2*fact3))%5)
--- Code Forces/tempCodeRunnerFile.cpp
+++ Code Forces/tempCodeRunnerFile.cpp
@@ -1,2 +1,1 @@
-)return low;
-        // else if(countZero(hig
+nCrModpLucas
