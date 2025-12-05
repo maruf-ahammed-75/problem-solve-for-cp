bd23c7d151d2c453d783c53bd576009340ca35a7
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2025-12-06 00:03:06 +0600

    CSES

--- Code Forces/1.cpp
+++ Code Forces/1.cpp
@@ -1,53 +1,47 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define endl "\n"
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+
 using namespace std;
 
-int p;                 // prime modulus
-vector<int> fact;      // factorials modulo p
+void I_Am_Here() {
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
 
-// Fast exponentiation: a^b % mod
-int modPow(int a, int b, int mod) {
-    int result = 1;
-    a %= mod;
-    while (b > 0) {
-        if (b & 1) result = (int)((1LL * result * a) % mod);
-        a = (int)((1LL * a * a) % mod);
-        b >>= 1;
+    int ans=0;
+    int temp=-1;
+    for(int i=0 ; i<n ; i++){
+        if(s[i]=='1'){
+            temp=0;
+            temp = i+k; 
+        }
+        else if(i>temp)ans++;  
+        // cout<<s[i]<<' '<<temp<<" "<<ans<<endl; 
     }
-    return result;
+    cout<<ans<<endl;
 }
 
-// Precompute factorials modulo p
-void precomputeFactorials(int prime) {
-    p = prime;
-    fact = vector<int>(p+1,1);
-    for (int i = 1; i < p; ++i)
-        fact[i] = ((1LL * fact[i - 1] * i) % p);
-}
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-// Compute nCr % p using modular inverse on the fly
-int nCr(int n, int r) {
-    if (r < 0 || r > n) return 0;
-    int numerator = fact[n];
-    int denominator = ((1LL * fact[r] * fact[n - r]) % p);
-    return ((1LL * numerator * modPow(denominator, p - 2, p)) % p);
-}
-
-// Lucas Theorem
-int nCrModpLucas(int n, int r) {
-    int res = 1;
-    while (n > 0 || r > 0) {
-        int ni = n % p;
-        int ri = r % p;
-        if (ri > ni) return 0; // if any digit ri > ni, result is 0
-        res = (int)((1LL * res * nCr(ni, ri)) % p);
-        n /= p;
-        r /= p;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t = 1;
+    cin >> t;
+    for (int T = 1; T <= t; T++) {
+        I_Am_Here();
     }
-    return res;
-}
-int main() {
-    int n = 100, r = 25, prime = 5;
-    precomputeFactorials(prime);
-    cout << "Value of nCr % p is " << nCrModpLucas(n, r) << endl;
     return 0;
 }
--- Code Forces/Niko-Tactical-Cards.cpp
+++ Code Forces/Niko-Tactical-Cards.cpp
@@ -9,32 +9,40 @@
 #define N cout<<"NO\n"
 
 using namespace std;
+int n;
+vector<int> a, b;
+vector<int> s(100005,0);
+int Findmax(int i){
+    if(i==n){
+        return s[i];
+    }
+    
+    int old = s[i];
+
+    //red 
+    s[i+1]=old - a[i];
+    int red = Findmax(i+1);
+    
+    //blue
+    s[i+1] = b[i] - old;
+    int blue = Findmax(i+1);
+    
+    return max(red,blue);
+}
 
 void I_Am_Here() {
-    int n;
     cin>>n;
-    int m;
-    cin>>m;
-    string s;
-    cin>>s;
-    int ans=0;
-    for(int i=0 ; i<n ; i++){
-        if(s[i]=='1'){
-            i+=m;
-        }
-        else{
-            ans++;
-        }
-    }
-    cout<<ans<<endl;
+    a = vector<int>(n);
+    b = vector<int>(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    for(int i=0;i<n;i++) cin>>b[i];
 
-
+    cout<<Findmax(0)<<endl;
 }
 
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
 
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
@@ -44,7 +52,6 @@ int32_t main() {
     int t = 1;
     cin >> t;
     for (int T = 1; T <= t; T++) {
-        // cout<<"Case "<<T<<": ";
         I_Am_Here();
     }
     return 0;
