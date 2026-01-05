a2799d7146d1e6ce76af8836e0b591b8fc97b667
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2026-01-05 22:52:54 +0800

    CF

--- Code Forces/Blackslex-and-Password.cpp
+++ Code Forces/Blackslex-and-Password.cpp
@@ -8,12 +8,50 @@
 #define Y cout<<"YES\n"
 #define N cout<<"NO\n"
 using namespace std;
+int n;
+vector<int>a;
+int minSum(int id , bool take = false){
+    // cout<<"id: "<<id<<endl;
+    if(id==n){
+        return 0;
+    }
+    int ans = LLONG_MAX;
 
+    //take 
+    ans = min (ans, minSum(id+1,take)+(id!=n-1?abs(a[id]-a[id+1]):0));
+
+    //not take
+    if(take == 0) ans = min(ans, minSum(id+1,1))-((id!=0)?abs(a[id]-a[id-1]):0)  +((id!=0 && id!=n-1)?abs(a[id+1]-a[id-1]):0);
+
+    // cout<<"ans: "<<ans<<endl;
+    return ans;
+}
 
 void I_Am_Here() {
-    int k,x;
-    cin>>k>>x;
-    cout<<k*x + 1<<endl;
+    
+    cin>>n;
+    a = vector<int>(n+1,0);
+    for(int i=0 ; i<n ; i++){
+        cin>>a[i];
+    }
+    int total = 0 ;
+    for(int i=0 ; i<n-1 ; i++){
+        total+= abs(a[i]-a[i+1]);
+    }
+
+    int ans = LLONG_MAX;
+    for(int i=0 ; i<n ; i++){
+        if(i==0){
+            ans = min(ans, total - abs(a[0]-a[1]));
+            continue;
+        }
+        if(i==n-1){
+            ans = min(ans, total - abs(a[n-1]-a[n-2]));
+            continue;
+        }
+        ans = min(ans,total - abs(a[i]-a[i-1]) - abs(a[i]-a[i+1]) + abs(a[i+1]-a[i-1]));
+    }
+    cout << ans << endl;
 }
 
 int32_t main() {
