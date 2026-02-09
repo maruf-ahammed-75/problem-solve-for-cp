36592299b6bb0b4d4a1a8ca2e4e9bfd7bb7d4951
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2026-02-09 16:40:13 +0600

    CF

---
+++ Code Forces/Journey-Planning.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+
+void I_Am_Here() {
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    map<int,int>m;
+    for(int i=0 ; i<n ; i++){
+        cin>>a[i];
+        m[a[i]-i]+=a[i];
+    }
+    int ans = 0;
+    for(auto i:m){
+        ans = max(ans , i.ss);
+    }
+    cout<<ans<<endl;
+
+}
+
+int32_t main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    // cin >> t;
+    for (int T = 1; T <= t; T++) {
+        I_Am_Here();
+    }
+    return 0;
+}
