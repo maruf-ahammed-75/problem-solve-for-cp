ba8e6792f075c447366ce994129168c568b88009
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2026-02-17 20:37:51 +0600

    CSES

--- Template/Graph/dsu.cpp
+++ Template/Graph/dsu.cpp
@@ -1,16 +1,18 @@
 int connected_components;
 int maximum_size;
 struct DSU {  
-vector<int> parent, component_size;
+vector<int> parent, component_size,rank;
     // O(N)
     DSU(int _n) {
         connected_components = _n;
         maximum_size = 1;
         parent = vector<int>(_n);
         component_size = vector<int>(_n);
+        rank = vector<int>(_n);
         for(int i = 0; i < _n; i++) {
             parent[i] = i;
             component_size[i] = 1;
+            rank[i] = 0;
         }
     }
     int find_parent(int a) {
@@ -22,6 +24,7 @@ vector<int> parent, component_size;
         }
     }
 
+    // by size
     bool unite(int a, int b) {
         a = find_parent(a);
         b = find_parent(b);
@@ -35,4 +38,18 @@ vector<int> parent, component_size;
 
         return true;
     }
+
+    //by rank
+    // bool unite(int a, int b) {
+    //     a = find_parent(a);
+    //     b = find_parent(b);
+    //     if(a == b) return false;
+
+    //     if(rank[a] < rank[b]) swap(a, b);
+    //     parent[b] = a;
+    //     component_size[a] += component_size[b];
+    //     if(rank[a] == rank[b]) rank[a]++;
+    //     return true;
+    // }
+        
 };
