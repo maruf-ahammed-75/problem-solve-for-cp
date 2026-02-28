83d5c0e9aaf4b616a74518a95eae589758a66d39
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2026-02-28 12:52:01 +0600

    CSES

---
+++ Code Forces/Sums.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define endl "\n"
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+
+void I_Am_Here() {
+    int n,x;
+    cin>>n>>x;
+    vector<int> a(n+1);
+    map<int,int>m;
+    m[0]=1;
+    for(int i=1 ; i<=n ; i++){
+        cin>>a[i];
+        a[i]+=a[i-1];
+        m[a[i]]++;
+    }
+    int ans =0 ;
+    for(int i=1 ; i<=n ; i++){
+        if(m[a[i]-x]){
+            ans++;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t = 1;
+    // cin >> t;   
+    for (int T = 1; T <= t; T++) {
+        I_Am_Here();
+    }
+    return 0;
+}
