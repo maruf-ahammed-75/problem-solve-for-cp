a06b0b7cbf776c7100e8c8cc2b13ae2f37835c9e
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2026-04-23 23:40:53 +0600

    SQL

--- Code Forces/1.cpp
+++ Code Forces/1.cpp
@@ -1,25 +1,43 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+void I_Am_Here() {
+    int n;
+    cin>>n;
+    // vector<int>a(n);
+    int m;
+    cin>>m;
 
-    int t;
-    cin >> t;
-
-    while (t--) {
-        long long n, m, a, b;
-        cin >> n >> m >> a >> b;
-
-        if (__gcd(n, a) == 1 &&
-            __gcd(m, b) == 1 &&
-            __gcd(n, b) == 1 &&
-            __gcd(m, a) == 1)
-            cout << "YES\n";
-        else
-            cout << "NO\n";
+    int x = m/n;
+    if(x==2){
+        N;
     }
+    else Y;
 
+
+}
+
+int32_t main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    cin >> t;
+    for (int T = 1; T <= t; T++) {
+        I_Am_Here();
+    }
     return 0;
 }
--- Code Forces/tempCodeRunnerFile.cpp
+++ Code Forces/tempCodeRunnerFile.cpp
@@ -1,55 +1,59 @@
-nt op = 1;
-    // while(q--){
-    //     // int test;
-    //     // cin>>test;
-    //     // int x,y;
-    //     // if(test == 1){
-            
-    //     //     cin>>x>>y;
-    //     //     if (stor[x].size() > stor[y].size())
-    //     //         swap(stor[x], stor[y]);
-    //     //     for(auto i:stor[x]){
-    //     //         stor[y].insert(i);
-    //     //         dsu.unite(y,i);
-    //     //     }
-    //     //     stor[x].clear();
-    //     // }
-    //     // else{
-    //     //     cin>>x;
-    //     //     cout<<dsu.find_parent(x)<<endl;
-    //     // }
-
-    //     // if(op== 3){
-    //     //     cout<<x<<" = ";
-    //     //     for(auto i:stor[x]){
-    //     //         cout<<i<<"  ";
-    //     //     }
-    //     //     cout<<endl<<endl;
-    //     // }
-
-    //     // cout<<"after op: "<<op++<<endl;
-    //     // for(int i=1 ; i<=n ; i++){
-    //     //     cout<<i<<"  = "<<dsu.find_parent(i)<<endl;
-    //     // }
-    //     // cout<<endl;
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define ff first
+#define ss second
+#define full(a) a.begin(), a.end()
+#define Y cout<<"YES\n"
+#define N cout<<"NO\n"
+using namespace std;
+string a,b;
+int n;
+vector<int>dp;
+int totalcost(int i){
+    if(i>=n)return 0;
+    if(dp[i]!=-1)return dp[i];
 
 
-    //     if(test == 1){
-    //         cin >> x >> y;
+    //virtical
+    int cost = (a[i]!=b[i]);
+    int ans = cost + totalcost(i+1);
 
-    //         if(x == y) continue;
+    //horizontal
+    if(i+1<n){
+        int cost2=0;
+        if(a[i]!=a[i+1])cost2++;
+        if(b[i]!=b[i+1])cost2++;
 
-    //         for(auto i: stor[x]){
-    //             a[i] = y;
-    //             stor[y].insert(i);
-    //         }
+        ans = min(ans,cost2 + totalcost(i+2) );
+    }
 
-    //         stor[x].clear();
-    //     }
-    //     else{
-    //         cin >> x;
-    //         cout << a[x] << endl;
-    //     }
+    return dp[i]=ans; 
+}
+void I_Am_Here() {
+    
+    cin>>n;
+    cin>>a>>b;
+    dp = vector<int>(n+10,-1);
+    cout<<totalcost(0)<<"\n";
 
-        
-    // }
+    
+}
+
+int32_t main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    cin >> t;
+    for (int T = 1; T <= t; T++) {
+        I_Am_Here();
+    }
+    return 0;
+}
