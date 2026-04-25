e1d2a13b8c18f90135eaf4543a144c60ec59c27a
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2026-04-25 19:00:19 +0600

    first approch

--- link list/Middle of the Linked List.cpp
+++ link list/Middle of the Linked List.cpp
@@ -1,3 +1,33 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode* temp = head;
+//         int total =0;
+//         while(temp != NULL){
+//             temp=temp->next;
+//             total++;
+//         }
+
+//         int mid = total/2 ;
+//         temp = head;
+//         for(int i=0 ; i<mid ; i++){
+//             temp=temp->next;
+//         }
+//         return temp;
+//     }
+// };
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -11,18 +41,21 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head;
-        int total =0;
-        while(temp != NULL){
-            temp=temp->next;
-            total++;
-        }
+        ListNode* fast = head;
+        ListNode* slow = head;
 
-        int mid = total/2 ;
-        temp = head;
-        for(int i=0 ; i<mid ; i++){
-            temp=temp->next;
+        while(fast != NULL){
+            fast = fast->next;
+            
+
+            if(fast!=NULL)fast = fast->next;
+            else break;
+            if (fast != NULL) {
+                cout << "fast val = "<<fast->val<<endl;
+            } 
+            slow = slow->next;
+            cout<<"slow val = "<<slow->val<<endl;
         }
-        return temp;
+        return slow;
     }
 };
