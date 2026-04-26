232d4107f2cfb11f66b026e73d350f6886587b49
maruf-ahammed-75 <ahammedmaruf007@gmail.com>
2026-04-26 06:30:30 +0600

    slow fast approch

--- link list/Linked List Cycle.cpp
+++ link list/Linked List Cycle.cpp
@@ -1,3 +1,34 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         ListNode* temp = head;
+//         set<ListNode*>s;
+//         while(temp != NULL){
+//             if(s.find(temp->next)!=s.end()){
+//                 for(auto i:s)cout<<i<<' ';
+//                 cout<<endl;
+//                 return true;
+//             }
+//             s.insert(temp->next);
+//             temp = temp->next;
+//         }
+//         for(auto i:s)cout<<i<<' ';
+//         cout<<endl;
+
+//         return 0;
+//     }
+// };
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,20 +40,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* temp = head;
-        set<ListNode*>s;
-        while(temp != NULL){
-            if(s.find(temp->next)!=s.end()){
-                for(auto i:s)cout<<i<<' ';
-                cout<<endl;
-                return true;
-            }
-            s.insert(temp->next);
-            temp = temp->next;
-        }
-        for(auto i:s)cout<<i<<' ';
-        cout<<endl;
+        ListNode* f = head;
+        ListNode* s=head;
 
-        return 0;
+        while(f->next != NULL && f!=NULL){
+            if(s==f)return 1;
+                s = s->next;
+                f = f->next;
+                f = f->next;
+        }
+        return false;
     }
 };
